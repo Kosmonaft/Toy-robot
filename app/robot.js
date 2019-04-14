349f53c99333a3223f2b1d664aeae5ec44d804e9
Pawel Christow <pawel.christow@gmail.com>
2019-04-14 18:06:43 +1000

    Change the report console to single line

--- app/robot.js
+++ app/robot.js
@@ -132,9 +132,7 @@ const moveRobot = () => {
 const reportPosition = () => {
     let currentPosition = appData.currentPosition;
     currentPosition[2] = appData.directions[currentPosition[2]];
-    console.log('****** ROBOT POSITION ******');
-    console.log('The ROBOT position is: ' + currentPosition.join(',').toUpperCase());
-    console.log('****************************');
+    console.log(`******** ROBOT POSITION ********\nThe position of the ROBOT  is: ${currentPosition.join(',').toUpperCase()}\n********************************`);
     toyRobot.askUser();
 }
 
